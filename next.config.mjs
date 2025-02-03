c8622a72adf32288f59ccc7b2d8976c9300354de
benpapac <ben.papac@gmail.com>
2025-02-03 13:09:50 -0500

    update yml, add imgix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 // import type { NextConfig } from "next";
 
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
+
+// module.exports = {
+// 	assetPrefix: assetPrefix,
+// 	basePath: basePath,
+// };
+
 const nextConfig = {
-  /* config options here */
+	/* config options here */
+	assetPrefix: assetPrefix,
+	basePath: basePath,
+	images: {
+		loader: 'imgix',
+		path: 'benpapac-sources.imgix.net',
+	},
 };
 
 export default nextConfig;
