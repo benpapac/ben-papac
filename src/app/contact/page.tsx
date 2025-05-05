7ab57d3e3fc665b263dd3dde94201858ccba14d5
benpapac <ben.papac@gmail.com>
2025-05-05 16:50:44 -0400

    contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -54,14 +54,9 @@ import Link from 'next/link';
     }
 
 	return (
-		<div className={"mt-4 flex flex-col md:flex-row place-content-center place-items-start p-8"}>
-			<div className={"mb-2"}>
-				<h1 className={"text-3xl "}>{"Let's get in touch."}</h1>
-				<p className={"txt-lg"}>{"To inquire about a project, or just say hello, just fill out the contact form."}</p>
-			</div>
-		<div className={"w-full md:w-1/3  h-3/4 mx-auto bg-slate-300 rounded-lg"} >
-			<form id={"contact-form"} className={"p-8 md:w-3/4 flex flex-col"} ref={form} onSubmit={sendEmail}>
-				<label className={" my-1 h-10 text-xl"}>Name</label>
+		<div className={"mt-[20%] md:mt-[10%] mx-auto flex flex-col place-items-center"}>
+			<form id={"contact-form"} className={"p-1 w-[90%] md:w-3/4 flex flex-col"} ref={form} onSubmit={sendEmail}>
+				<label className={"my-1 h-10 text-xl"}>Name</label>
 				<input type="hidden" name='to_name' value="Ben"/>
 				<input className={"md:w-3/4 rounded shadow appearance-none h-10 p-1"} type='text' placeholder='e.g. Nicole Seymour' name='name' />
 				<label className={"my-1 h-10 text-xl"}>Email Address</label>
@@ -72,7 +67,7 @@ import Link from 'next/link';
 				<textarea className={"rounded shadow appearance-none p-1"} rows={6} name='message' placeholder="Type your message here." />
 				<input className={" mt-10 w-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"} type='submit' value='Send.' />
 			</form>
-		</div>
+
 		</div>
 	);
 };
